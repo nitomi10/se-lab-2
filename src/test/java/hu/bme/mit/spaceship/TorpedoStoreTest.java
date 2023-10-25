e5333a0e7371e18f450c6c267bae0796db2d932b
unknown <176040@BME.HU>
2023-10-25 17:58:31 +0200

    JUnit tests added

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.doubleThat;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,9 +22,14 @@ class TorpedoStoreTest {
   @Test
   void fire_Fail(){
     TorpedoStore store = new TorpedoStore(0);
-    
-    boolean result = store.fire(1);
-
+    boolean result;
+    try{
+     result = store.fire(1);
+     result = store.fire(0);
+     result = store.fire(-1);
+    }catch(IllegalArgumentException e){
+      result = false;
+    }
     assertEquals(false, result);
 
     store = new TorpedoStore(1, 1);
@@ -32,10 +38,30 @@ class TorpedoStoreTest {
   }
   @Test
   void torpedo_faliure(){
-    TorpedoStore store = new TorpedoStore(10000, 0.5);
-    
-    for(int i = 0; i<10000;i++){
-    
+    int try_num = 10000;
+    double faliure = 0.5;
+    double epsilon = 0.1;
+    TorpedoStore store = new TorpedoStore(try_num, faliure);
+    double success_num = 0;
+    for(int i = 0; i<try_num;i++){
+      boolean success = store.fire(1);
+      if (success){
+        success_num++;
+      }
     }
+    double all_trys = try_num;
+    
+    assertEquals( faliure,(success_num/all_trys), epsilon );
+  }
+  @Test
+  void torpedo_store(){
+    TorpedoStore store = new TorpedoStore(1);
+    int num = store.getTorpedoCount();
+    assertEquals(1,num);
+    boolean result = store.isEmpty();
+    assertEquals(false,result);
+    store.fire(1);
+    result = store.isEmpty();
+    assertEquals(true,result);
   }
 }
