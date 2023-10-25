a7f79ed0a7b9fb590c5b7b026ace0c2e39e37d9d
unknown <176040@BME.HU>
2023-10-25 17:36:58 +0200

    adedd tests

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -8,13 +8,34 @@ class TorpedoStoreTest {
 
   @Test
   void fire_Success(){
-    // Arrange
     TorpedoStore store = new TorpedoStore(1);
 
-    // Act
     boolean result = store.fire(1);
 
-    // Assert
     assertEquals(true, result);
+    
+    store = new TorpedoStore(10, 0);
+    result = store.fire(store.getTorpedoCount());
+    assertEquals(true, result);
+  }
+  @Test
+  void fire_Fail(){
+    TorpedoStore store = new TorpedoStore(0);
+    
+    boolean result = store.fire(1);
+
+    assertEquals(false, result);
+
+    store = new TorpedoStore(1, 1);
+    result = store.fire(1);
+    assertEquals(false, result);
+  }
+  @Test
+  void torpedo_faliure(){
+    TorpedoStore store = new TorpedoStore(10000, 0.5);
+    
+    for(int i = 0; i<10000;i++){
+    
+    }
   }
 }
